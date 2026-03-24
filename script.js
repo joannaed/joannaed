4e08c3099029f16e76e0566a65f70474af7f346a
joannaed <j.plachcinska@onet.pl>
2026-03-24 12:32:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+document.body.classList.toggle("dark");
+
+if (document.body.classList.contains("dark")) {
+localStorage.setItem("theme", "dark");
+} else {
+localStorage.setItem("theme", "light");
+}
+}
+
+window.onload = function () {
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+document.body.classList.add("dark");
+}
+};
